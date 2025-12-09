b4c28ccaf091d9f00b3404be59933d4b57b2c088
c0a24093 <c0a24093ab@edu.teu.ac.jp>
2025-12-09 18:07:38 +0900

    電磁パルスマージバグ修正

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -297,7 +297,7 @@ class Score:
     def __init__(self):
         self.font = pg.font.Font(None, 50)
         self.color = (0, 0, 255)
-        self.value = 0
+        self.value = 1000
         self.image = self.font.render(f"Score: {self.value}", 0, self.color)
         self.rect = self.image.get_rect()
         self.rect.center = 100, HEIGHT-50
@@ -478,11 +478,6 @@ def main():
                 exps.add(Explosion(bomb, 50))  # 爆発エフェクト
                 score.value += 1  # 1点アップ
                 continue
-            bird.change_img(8, screen)  # こうかとん悲しみエフェクト
-            score.update(screen)
-            pg.display.update()
-            time.sleep(2)
-            return
             if bomb.state == "active":
                 bird.change_img(8, screen)  # こうかとん悲しみエフェクト
                 score.update(screen)
@@ -491,6 +486,7 @@ def main():
                 return
             else:
                 pass
+            
 
         bird.update(key_lst, screen)
         beams.update()
